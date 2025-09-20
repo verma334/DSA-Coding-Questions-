70ce66b4db15a90e92b3217bd12501d32e969458
Aditi Verma <verm334@gmail.com>
2025-09-20 18:07:06 +0530

    wave array

---
+++ WaveArray.java
@@ -0,0 +1,25 @@
+package ArrayAndStringsDSAQuestions;
+
+public class WaveArray {
+
+	
+	void sortInWave(int arr[]){
+        int n = arr.length;
+     //   int n =arr.length;
+        // swap adjacent elements
+        for (int i=0; i<n-1; i += 2){
+            int temp = arr[i];
+            arr[i] = arr[i+1];
+            arr[i+1] = temp;
+        }
+    }
+
+    public static void main(String args[]){
+    	WaveArray wv = new WaveArray();
+        int arr[] = {1, 2, 3, 4, 5};
+        wv.sortInWave(arr);
+        for (int i : arr){
+            System.out.print(i + " ");
+        }
+}
+}
