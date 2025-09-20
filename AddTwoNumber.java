c75df76404cd3567ef1f627fd84ec41b1261bbe0
Aditi Verma <verm334@gmail.com>
2025-09-20 18:56:04 +0530

    add two numbert

---
+++ AddTwoNumber.java
@@ -0,0 +1,19 @@
+package MathQuestionsDSA;
+
+public class AddTwoNumber {
+
+	
+	 public static int addition(int a,int b){
+	        return a+b;
+	    }
+	    
+	    public static void main (String[] args){
+	        int a = 1;
+	        int b = 2;
+	        
+	    System.out.println("Addition of " + a + "and" + b + " is " + addition(a,b));
+	    // System.out.println("Addition of " + a + " and " + b + " is " + addition(a, b));
+	    
+	        
+	    }
+}
