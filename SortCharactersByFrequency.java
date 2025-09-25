5b48950d7de850ec01a45b9a77bd16679a223f08
Aditi Verma <verm334@gmail.com>
2025-09-25 13:40:10 +0530

    added frequency

---
+++ SortCharactersByFrequency.java
@@ -0,0 +1,42 @@
+package ArrayAndStringsDSAQuestions;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class SortCharactersByFrequency {
+
+	
+	////adding by frequency////////////////////
+	public String frequencySort(String s) {
+        Map<Character,Integer> map =new HashMap<>();
+        for(int i=0;i<s.length();i++){
+           char ch = s.charAt(i);
+           if(map.containsKey(ch)){
+            map.put(ch,map.get(ch) + 1);
+           }else{
+            map.put(ch,1);
+           }
+        }
+
+        //////////sorting by frequency////////////
+      List<Character> chars = new ArrayList<>(map.keySet());
+      Collections.sort(chars, (a,b) -> map.get(b) - map.get(a));
+      StringBuilder sb = new StringBuilder();
+      for(Character ch: chars){
+        for(int i=0;i<map.get(ch);i++){
+          sb.append(ch);
+        }
+      }
+      return sb.toString();
+    }
+	
+	public static void main(String args[]) {
+		SortCharactersByFrequency sc = new SortCharactersByFrequency();
+		String s = "cccaaa";
+		String result =sc.frequencySort(s);
+		System.out.println("Count string by frequerncy:" + result);
+	}
+}
